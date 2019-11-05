4fa823d0aa627421837af38b8400850634188d42
Kenneth Julian <kennethjulian026@gmail.com>
2019-11-04 22:01:52 -0600

    added new files

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
     belongs_to :user
+    validates :message, presence: true
+    validates :message, length: {maximum: 220, too_long: "A tweet is only 220 max."}
 end
